30f38f25c20d7aa0f047e917aab6543061d5dd8c
Nauris Vasilevskis <navris@inbox.lv>
2023-10-25 23:57:30 +0300

    spi initialization done

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
+#include "driver/spi_master.h"
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
 char msgString[128];    
+
+spi_device_handle_t spi2;
 
 gpio_num_t CAN0_INT = GPIO_NUM_18;
 gpio_num_t CAN0_CS = GPIO_NUM_19;
@@ -30,8 +32,36 @@ void app_main() {
     vTaskDelay((TickType_t)30000);
     gpio_set_level(GPIO_NUM_15, 1);
     
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, CAN0_CS);
+
+    spi_bus_config_t buscfg={
+        .mosi_io_num = SPI_MOSI,
+        .miso_io_num = SPI_MISO,
+        .sclk_io_num = SPI_SCK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 32,
+    };
+    esp_err_t ret;
+    ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    //SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, CAN0_CS);
+    ESP_ERROR_CHECK(ret);
     
+    spi_device_interface_config_t devcfg={
+        
+        .mode = 0,                  //SPI mode 0
+        .spics_io_num = CAN0_CS,
+        .flags = SPI_DEVICE_HALFDUPLEX,
+             
+        .queue_size = 1,
+        
+        .pre_cb = NULL,
+        .post_cb = NULL,
+        //.clock_speed_hz = 1000000,  // 1 MHz
+    };
+
+    ESP_ERROR_CHECK(spi_bus_add_device(SPI2_HOST, &devcfg, &spi2));
+
+
       // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
     if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
         printf("MCP2515 initialization successful! \n");
