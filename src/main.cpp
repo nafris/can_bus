337fe7945d0d2390caf6290b08d37abd525e2d8e
Nauris Vasilevskis <navris@inbox.lv>
2023-10-27 22:30:31 +0300

    first code ready to test on hardware

--- src/main.cpp
+++ src/main.cpp
@@ -81,32 +81,33 @@ void app_main() {
     
     }
 
-    /*
-      // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+    
+      // Initialize MCP2515 running at 8MHz with a baudrate of 500kb/s and the masks and filters disabled.
     if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
         printf("MCP2515 initialization successful! \n");
     else
         printf("Error initializing MCP2515! \n");
     CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-    gpio_set_direction(GPIO_NUM_16, GPIO_MODE_INPUT);
+    gpio_set_direction(CAN0_INT, GPIO_MODE_INPUT);
     printf("MCP2515 Library Receive Example...\n");
     
     while(1){
         if(!gpio_get_level(CAN0_INT)){                         // If CAN0_INT pin is low, read receive buffer
-        CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-        
-        if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-            printf("Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
-        else
-            printf("Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+            CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+            
+            if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+                printf("Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+            else
+                printf("Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
 
-        if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
-            printf(" REMOTE REQUEST FRAME");
-        }else{
-            for(uint8_t i = 0; i < len; i++){
-                printf(" 0x%.2X", rxBuf[i]);
-            }
-        }        
-        printf("\n");
-    }*/
+            if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+                printf(" REMOTE REQUEST FRAME");
+            }else{
+                for(uint8_t i = 0; i < len; i++){
+                    printf(" 0x%.2X", rxBuf[i]);
+                }
+            }        
+            printf("\n");
+        }
+    }
 }
