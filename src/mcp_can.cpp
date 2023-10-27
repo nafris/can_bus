2a557598bd806d5dc8423cd70037fa1bbe0bb2d2
Nauris Vasilevskis <navris@inbox.lv>
2023-10-27 19:02:37 +0300

    removed spi init from class

--- src/mcp_can.cpp
+++ src/mcp_can.cpp
@@ -911,7 +911,7 @@ INT8U MCP_CAN::begin(INT8U idmodeset, INT8U speedset, INT8U clockset)
 {
     INT8U res;
 
-    mcpSPI->begin();
+    //mcpSPI->begin(); spi initialization goes here if moved to inside class
     res = mcp2515_init(idmodeset, speedset, clockset);
     if (res == MCP2515_OK)
         return CAN_OK;
